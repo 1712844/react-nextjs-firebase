677f14e07c510d4f9ac0c263a48af65a258bf6ca
wippy <98chitrungnd@gmail.com>
2022-08-21 16:02:07 +0100

    rehydrate post content

---
+++ components/PostContent.tsx
@@ -0,0 +1,24 @@
+import Link from '../node_modules/next/link';
+import ReactMarkdown from '../node_modules/react-markdown/index';
+
+export default function PostContent({ post }) {
+  const createdAt =
+    typeof post?.createdAt === 'number'
+      ? new Date(post.createdAt)
+      : post.createdAt.toDate();
+
+  return (
+    <div className="card">
+      <h1>{post?.title}</h1>
+      <span className="text-sm">
+        Written by{' '}
+        <Link href={`/${post.username}`}>
+          <a className="text-info">@{post.username}</a>
+        </Link>{' '}
+        on {createdAt.toISOSString()}
+      </span>
+
+      <ReactMarkdown>{post?.content}</ReactMarkdown>
+    </div>
+  );
+}
--- pages/[username]/[slug].tsx
+++ pages/[username]/[slug].tsx
@@ -1,3 +1,62 @@
-export default function UserSlugPage({}) {
-  return <main></main>;
+import { firestore } from '../../lib/firebase';
+import { getUserWithUsername, postToJSON } from '../../lib/getUserWithUsername';
+import { userDocumentData } from 'react-firebase-hooks/firestore';
+import styles from '../../styles/Post.module.css';
+import PostContent from '../../components/PostContent';
+
+export async function getStaticProps({ params }) {
+  const { username, slug } = params;
+  const userDoc = await getUserWithUsername(username);
+
+  let post;
+  let path;
+
+  if (userDoc) {
+    const userRef = userDoc.ref.collection('posts').doc(slug);
+    post = postToJSON(await userRef.get());
+
+    path = userRef.path;
+  }
+
+  return {
+    props: { post, path },
+    revalidate: 5000,
+  };
+}
+
+export async function getStaticPaths() {
+  const snapshot = await firestore.collectionGroup('posts').get();
+
+  const paths = snapshot.docs.map((doc) => {
+    const { slug, username } = doc.data();
+    return {
+      params: { username, slug },
+    };
+  });
+
+  return {
+    paths,
+    fallback: 'blocking',
+  };
+}
+
+export default function Post(props) {
+  const postRef = firestore.doc(props.path);
+  const [realtimePost] = useDocumentData(postRef);
+
+  const post = realtimePost || props.post;
+
+  return (
+    <main className={styles.container}>
+      <section>
+        <PostContent post={post} />
+      </section>
+
+      <aside className="card">
+        <p>
+          <strong>{post.heartCount || 0} 🤍</strong>
+        </p>
+      </aside>
+    </main>
+  );
 }
